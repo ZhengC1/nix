500454b1c6770a83d0f2bdfa077a6a20c29367d7
Chun Zheng <zhengc42@gmail.com>
2026-06-10 11:38:16 +0000

    added homemanager configs

---
+++ home.nix
@@ -0,0 +1,13 @@
+{ lib, pkgs, ... };
+
+{
+  home = {
+    packages = with pkgs; [
+      hello
+    ];
+    username = "wizheng";
+    homeDirectory = "/home/wizheng";
+    stateVersion = "23.11";
+  };
+
+}
