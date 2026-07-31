ccea13e1c0b290edc580f5af52a7f0e85655ba80
Chun Zheng <zhengc42@gmail.com>
2026-07-31 10:48:19 -0400

    tmux

--- config/tmux.nix
+++ config/tmux.nix
@@ -1,39 +1,17 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
   programs.tmux = {
     enable = true;
-    prefix = "C-a";
-    baseIndex = 1;
-    historyLimit = 10000;
-    mouse = true;
-    plugins = with pkgs.tmuxPlugins; [
-      sensible
-      resurrect
-    ];
-
-    extraConfig = ''
-      # Unbind default split keys
-      unbind '"'
-      unbind %
-
-      # Split panes using | and -
-      bind | split-window -h
-      bind - split-window -v
-
-      # Reload config file
-      bind r source-file ~/.config/tmux/tmux.conf
-
-      # Switch panes using Alt-arrow without prefix
-      bind -n M-h select-pane -L
-      bind -n M-l select-pane -R
-      bind -n M-k select-pane -U
-      bind -n M-j select-pane -D
-    '';
   };
 
-  # Install tpm (tmux plugin manager) for custom plugins
+  # Install tmux and tpm
   home.packages = with pkgs; [
     tmux
   ];
+
+  # Create tmux config file in home directory
+  home.file.".tmux.conf" = {
+    source = ../dotfiles/.tmux.conf;
+  };
 }
